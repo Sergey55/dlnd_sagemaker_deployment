75d948c73583ad835a6ab92d44a1a7d65a6bdd5c
Sergey Andreev <sergey666andreev@gmail.com>
2020-06-22 14:42:01 +0600

    feat: Do not remove stop words

--- serve/predict.py
+++ serve/predict.py
@@ -91,13 +91,13 @@ def predict_fn(input_data, model):
     return result
 
 def review_to_words(review):
-    nltk.download("stopwords", quiet=True)
+    # nltk.download("stopwords", quiet=True)
     stemmer = PorterStemmer()
     
     text = BeautifulSoup(review, "html.parser").get_text() # Remove HTML tags
     text = re.sub(r"[^a-zA-Z0-9]", " ", text.lower()) # Convert to lower case
     words = text.split() # Split string into words
-    words = [w for w in words if w not in stopwords.words("english")] # Remove stopwords
+    # words = [w for w in words if w not in stopwords.words("english")] # Remove stopwords
     words = [PorterStemmer().stem(w) for w in words] # stem
     
     return words
